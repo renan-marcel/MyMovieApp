8e9e8ead54b86dc0485146762a3d5a56f4a49f3b
renan-marcel <renanmarcelpaulinoleite@gmail.com>
2025-06-18 15:03:02 -0300

    Remove obsolete unit tests and related files from MyMovieApp.Infrastructure.Tests and MyMovieApp.Application.Tests. This includes the deletion of UnitTest1.cs, DbInitializerTests.cs, OmdbMovieProviderTests.cs, MovieRepositoryTests.cs, and the associated test setup code to streamline the test suite and improve maintainability.

--- src/MyMovieApp.Application/Services/IMovieService.cs
+++ src/MyMovieApp.Application/Services/IMovieService.cs
@@ -6,6 +6,7 @@ namespace MyMovieApp.Application.Interfaces;
 public interface IMovieService
 {
     Task<Movie> GetMovieByImdbIdAsync(CancellationToken cancellationToken, string imdbId);
+
     Task<Movie> GetMovieByTitleAsync(CancellationToken cancellationToken, SearchRequestDto searchRequest);
 
     Task<Movie> CreateMovieReviewAsync(CancellationToken cancellationToken, string imdbId, string userOpinion,
